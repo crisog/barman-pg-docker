ada72cc103333f096f61f46fefbcedf3ffba0508
crisog <ortega.cpp@gmail.com>
2025-04-21 09:39:13 -0400

    Add initial Barman and PostgreSQL setup with Docker

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # barman-pg-docker
+
+This repo is based off softwarebrahma's [PostgreSQL-Disaster-Recovery-With-Barman](https://github.com/softwarebrahma/PostgreSQL-Disaster-Recovery-With-Barman/tree/master)
---
+++ postgres/docker-entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+function customize {
+    # Create SSH directory if it doesn't exist
+    mkdir -p /root/.ssh
+    
+    # Use environment variables for SSH keys if provided
+    if [ ! -z "$SSH_PRIVATE_KEY" ]; then
+        echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
+        echo "$SSH_PUBLIC_KEY" > /root/.ssh/id_rsa.pub
+        echo "$SSH_PUBLIC_KEY" > /root/.ssh/authorized_keys
+    else
+        # Generate SSH keys if not provided via env vars
+        ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
+        cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+    fi
+    
+    # Set proper permissions
+    chmod 700 /root/.ssh
+    chmod 644 /root/.ssh/id_rsa.pub
+    chmod 600 /root/.ssh/id_rsa
+    chmod 600 /root/.ssh/authorized_keys
+
+    # Do the same for postgres user
+    su - postgres -c "mkdir -p ~postgres/.ssh
+        if [ ! -z \"$SSH_PRIVATE_KEY\" ]; then
+            echo \"$SSH_PRIVATE_KEY\" > ~postgres/.ssh/id_rsa
+            echo \"$SSH_PUBLIC_KEY\" > ~postgres/.ssh/id_rsa.pub
+            echo \"$SSH_PUBLIC_KEY\" > ~postgres/.ssh/authorized_keys
+        else
+            ssh-keygen -t rsa -N \"\" -f ~postgres/.ssh/id_rsa
+            cp ~postgres/.ssh/id_rsa.pub ~postgres/.ssh/authorized_keys
+        fi
+        chmod 700 ~postgres/.ssh
+        chmod 644 ~postgres/.ssh/id_rsa.pub
+        chmod 600 ~postgres/.ssh/id_rsa
+        chmod 600 ~postgres/.ssh/authorized_keys"
+
+    # Start SSH server
+    /usr/sbin/sshd 2>&1
+}
+
+# Run custom setup and then the standard PostgreSQL entrypoint
+customize & /usr/local/bin/docker-entrypoint.sh "$@"
