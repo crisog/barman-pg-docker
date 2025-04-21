8038b5e5d1ecaff1b7f9d6dd6582fd5b63d4201b
crisog <ortega.cpp@gmail.com>
2025-04-21 16:05:38 -0400

    refactor: streamline initialization and configuration scripts by removing logging and redundant checks

--- postgres/02-db-config.sh
+++ postgres/02-db-config.sh
@@ -2,11 +2,8 @@
 #!/bin/bash
 set -e
 
-echo "[$(date)] Starting 02-db-config.sh"
-
 # Create a superuser 'barman' for backups, with an MD5‐hashed password
 # (uses POSTGRES_PASSWORD + username for the md5 hash prefix)
-echo "[$(date)] Creating barman superuser"
 BARMAN_USER=barman
 BARMAN_PASS=$(echo -n "md5${POSTGRES_PASSWORD}${BARMAN_USER}" | md5sum | cut -d' ' -f1)
 
@@ -14,16 +11,11 @@ psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
   CREATE USER $BARMAN_USER WITH SUPERUSER PASSWORD '$BARMAN_PASS';
   GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO $BARMAN_USER;
 EOSQL
-echo "[$(date)] Successfully created barman user and granted privileges"
 
 # Ensure your main user is superuser and has the right password/privileges
-echo "[$(date)] Setting up main Postgres user privileges"
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
   ALTER USER \"$POSTGRES_USER\" WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
 "
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
   GRANT ALL PRIVILEGES ON DATABASE \"$POSTGRES_DB\" TO \"$POSTGRES_USER\";
 "
-echo "[$(date)] Successfully set up main Postgres user privileges"
-
-echo "[$(date)] Completed 02-db-config.sh"
--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -1,67 +1,44 @@
 #!/bin/bash
 set -e
 
-echo "[$(date)] Starting docker-entrypoint.sh"
-
 # Validate PGDATA is set
 if [ -z "$PGDATA" ]; then
-  echo "[$(date)] ERROR: Missing PGDATA variable" >&2
   exit 1
 fi
-echo "[$(date)] PGDATA is set to: $PGDATA"
-
-# Function to check if SSH key is valid (contains PEM header)
-is_valid_ssh_key() {
-  echo "$1" | grep -q "BEGIN.*PRIVATE KEY"
-}
 
 customize() {
-  echo "[$(date)] Starting customize function"
-  
   # Root SSH key setup
-  echo "[$(date)] Setting up root SSH keys"
   mkdir -p /root/.ssh
-  if [ -n "$SSH_PRIVATE_KEY" ] && is_valid_ssh_key "$SSH_PRIVATE_KEY"; then
-    echo "[$(date)] Using provided SSH keys for root"
+  if [ -n "$SSH_PRIVATE_KEY" ]; then
     echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
     echo "$SSH_PUBLIC_KEY"  > /root/.ssh/id_rsa.pub
     echo "$SSH_PUBLIC_KEY"  > /root/.ssh/authorized_keys
   else
-    echo "[$(date)] Generating new SSH keys for root"
     ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
     cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
   fi
   chmod 700 /root/.ssh && chmod 600 /root/.ssh/id_rsa* && chmod 600 /root/.ssh/authorized_keys
-  echo "[$(date)] Root SSH setup complete"
 
-  # Postgres user SSH setup
-  echo "[$(date)] Setting up postgres user SSH keys"
-  su - postgres -c "mkdir -p ~postgres/.ssh
-    if [ -n \"$SSH_PRIVATE_KEY\" ] && is_valid_ssh_key \"$SSH_PRIVATE_KEY\"; then
-      echo \"[$(date)] Using provided SSH keys for postgres user\"
-      echo \"$SSH_PRIVATE_KEY\" > ~postgres/.ssh/id_rsa
-      echo \"$SSH_PUBLIC_KEY\"  > ~postgres/.ssh/id_rsa.pub
-      echo \"$SSH_PUBLIC_KEY\"  > ~postgres/.ssh/authorized_keys
+  # Postgres user SSH setup - pass environment variables explicitly
+  su - postgres -c "
+    mkdir -p ~/.ssh
+    if [ -n '$SSH_PRIVATE_KEY' ]; then
+      echo '$SSH_PRIVATE_KEY' > ~/.ssh/id_rsa
+      echo '$SSH_PUBLIC_KEY'  > ~/.ssh/id_rsa.pub
+      echo '$SSH_PUBLIC_KEY'  > ~/.ssh/authorized_keys
     else
-      echo \"[$(date)] Generating new SSH keys for postgres user\"
-      ssh-keygen -t rsa -N \"\" -f ~postgres/.ssh/id_rsa
-      cp ~postgres/.ssh/id_rsa.pub ~postgres/.ssh/authorized_keys
+      ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+      cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
     fi
-    chmod 700 ~postgres/.ssh && chmod 600 ~postgres/.ssh/id_rsa* && chmod 600 ~postgres/.ssh/authorized_keys"
-  echo "[$(date)] Postgres user SSH setup complete"
+    chmod 700 ~/.ssh && chmod 600 ~/.ssh/id_rsa* && chmod 600 ~/.ssh/authorized_keys
+  "
 
   # Start SSH daemon
-  echo "[$(date)] Starting SSH daemon"
   /usr/sbin/sshd
-  echo "[$(date)] SSH daemon started"
-  
-  echo "[$(date)] Customize function completed"
 }
 
 # Run SSH setup in background
-echo "[$(date)] Running SSH setup in background"
 customize &
 
 # Finally, exec Railway's SSL wrapper (initdb, init-ssl.sh, then postgres)
-echo "[$(date)] Executing wrapper.sh: $@"
 exec /usr/local/bin/wrapper.sh "$@"
