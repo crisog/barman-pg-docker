5557afc9091301b989a1891e3f7f5530fdbcd0f4
crisog <ortega.cpp@gmail.com>
2025-04-22 17:33:22 -0400

    refactor: modify PostgreSQL configuration script to disable archive mode, enhancing performance for non-archiving setups

--- postgres/common/01-init-custom-config.sh
+++ postgres/common/01-init-custom-config.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-# ensure PGDATA is set
 [ -z "$PGDATA" ] && exit 1
 
-# copy in your pg_hba and update perms
 cp /var/lib/postgresql/config/pg_hba.conf "$PGDATA/pg_hba.conf"
 chown postgres:postgres "$PGDATA/pg_hba.conf"
 
-# append core settings
 cat <<EOF >> "$PGDATA/postgresql.conf"
 listen_addresses = '*'
 wal_level = replica
-archive_mode = on
+archive_mode = off
 max_wal_senders = 2
 max_replication_slots = 2
 hba_file = '$PGDATA/pg_hba.conf'
