08e317fbefa45e95255f9793c0ee40141c004ab7
crisog <ortega.cpp@gmail.com>
2025-08-22 01:15:22 -0400

    optimize PostgreSQL configuration with enhanced WAL settings and monitoring

--- postgres/common/01-init-custom-config.sh
+++ postgres/common/01-init-custom-config.sh
@@ -9,10 +9,17 @@ chown postgres:postgres "$PGDATA/pg_hba.conf"
 cat <<EOF >> "$PGDATA/postgresql.conf"
 listen_addresses = '*'
 wal_level = replica
+wal_compression = on
+wal_buffers = 16MB
 archive_mode = off
-max_wal_senders = 2
-max_replication_slots = 2
+archive_timeout = 60s
+max_wal_senders = 10
+max_replication_slots = 10
 hba_file = '$PGDATA/pg_hba.conf'
+checkpoint_completion_target = 0.9
+shared_preload_libraries = 'pg_stat_statements'
+track_io_timing = on
+track_functions = all
 EOF
 
 chown postgres:postgres "$PGDATA/postgresql.conf"
